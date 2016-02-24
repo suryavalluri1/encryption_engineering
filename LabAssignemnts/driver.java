821a39d7cf34592e2f0ccf2a9fd06f598584c323
suryavalluri1 <surya.valluri1@marist.edu>
2016-02-23 21:38:27 -0500

    Surya Valluri, 02/23/2016, AesCipher LabAssignment-2
    Code with final changes and indentation/styles.

--- LabAssignemnts/driver.java
+++ LabAssignemnts/driver.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
+/**
+ * This class takes the user input key and calls aescipher class for processing
+ * the keys.
+ * 
+ * @author siddhartha
+ *
+ */
 
 public class driver {
 
-	public static void main(String args[])
-	{
-		 System.out.println("Enter the key");
-		 Scanner sc = new Scanner(System.in);
-	     String input = sc.nextLine();
-	    
-	     aescipher as = new aescipher();
-	     as.processInput(input);
-	     
-	     
-	 
-	}
-	
-	
+  public static void main(String args[]) {
+
+    Scanner sc = new Scanner(System.in);
+    String input = sc.nextLine();
+    aescipher as = new aescipher();
+    input = input.trim();
+    as.processInput(input);
+
+  }
+
 }
